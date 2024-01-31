11644354e41c46f87e75352bdef37cdda74a3b29
Andreas Lindell <andreas.lindell97@gmail.com>
2024-01-31 13:01:01 +0100

    First Commit for Frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ShiftsComponent } from './shifts/shifts.component';
+import { BranchesComponent } from './branches/branches.component';
+import { EmployeesComponent } from './employees/employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Dashboard', component: DashboardComponent},
+  {path: 'Shifts', component: ShiftsComponent},
+  {path: 'Branches', component: BranchesComponent},
+  {path: 'Employees', component: EmployeesComponent},
+  {path: '', redirectTo: 'Dashboard', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './nav/sidebar/sidebar.component';
+import { ShiftsComponent } from './shifts/shifts.component';
+import { BranchesComponent } from './branches/branches.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    DashboardComponent,
+    SidebarComponent,
+    ShiftsComponent,
+    BranchesComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
